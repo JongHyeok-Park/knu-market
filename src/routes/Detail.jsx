6e1bda680c892a15aa7c3c0c8c9ab1cd4845f5fa
Jonghyeok <jhpmuscle@gmail.com>
2024-05-24 03:00:41 +0900

    feat: add delete, modify btn

--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -1,11 +1,14 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import './Detail.css';
 import { useEffect, useState } from 'react';
+import { deleteProductApi } from '../api/productApi';
+import { useSelector } from 'react-redux';
 
 function Detail(props) {
   const params = useParams('id');
   const navigate = useNavigate();
   let [productInfo, setProductInfo] = useState(null);
+  let user = useSelector(state => state.user);
 
   const getDetailInfo = async () => {
     await fetch(process.env.REACT_APP_API_URL + '/api/product/' + params.id)
@@ -39,40 +42,59 @@ function Detail(props) {
         productInfo ? 
         (
           <div className='detail-inner'>
-              <div className='detail-info'>
-                <div className='detail-info-left'>
-                  {
-                    productInfo.imagePath ? 
-                    <img src={productInfo.imagePath} alt="product" /> :
-                    "이미지가 없어요"
-                  }
+            {
+              user.name ? (
+                <div className='detail-manage-btn-container'>
+                  <button id='edit-btn' className='btn' onClick={() => {
+                    navigate('/modify/' + params.id);
+                  }}>수정하기</button>
+                  <button id='delete-btn' className='btn' onClick={() => {
+                    deleteProductApi(params.id)
+                    .then(() => {
+                      navigate('/');
+                    })
+                    .catch((err) => {
+                      alert(err.message);
+                    })
+                  }}>삭제하기</button>
                 </div>
-                <div className='detail-info-right'>
-                  <div className='detail-product-info'>
-                    <span className='info-date'>{productInfo.createdAt}</span>
-                    <h2>{productInfo.title}</h2>
-                    <h1>{Number(productInfo.price).toLocaleString()}원</h1>
-                    <p>{productInfo.description}</p>
+              ) : null
+            }
+            
+            <div className='detail-info'>
+              <div className='detail-info-left'>
+                {
+                  productInfo.imagePath ? 
+                  <img src={productInfo.imagePath} alt="product" /> :
+                  "이미지가 없어요"
+                }
+              </div>
+              <div className='detail-info-right'>
+                <div className='detail-product-info'>
+                  <span className='info-date'>{productInfo.createdAt}</span>
+                  <h2>{productInfo.title}</h2>
+                  <h1>{Number(productInfo.price).toLocaleString()}원</h1>
+                  <p>{productInfo.description}</p>
+                </div>
+                <div className='detail-info-user'>
+                  <div className='top'>
+                    <div className='user-image-wrapper'>
+                      {
+                        productInfo.userImagePath ?
+                        <img src={productInfo.userImagePath} alt="user" /> :
+                        <img src={require('../image/user_icon.png')} alt="user" />
+                      }
+                    </div>
+                    <span>
+                      <h3>{productInfo.userName}</h3>
+                    </span>
                   </div>
-                  <div className='detail-info-user'>
-                    <div className='top'>
-                      <div className='user-image-wrapper'>
-                        {
-                          productInfo.userImagePath ?
-                          <img src={productInfo.userImagePath} alt="user" /> :
-                          <img src={require('../image/user_icon.png')} alt="user" />
-                        }
-                      </div>
-                      <span>
-                        <h3>{productInfo.userName}</h3>
-                      </span>
-                    </div>
-                    <div className='bottom'>
+                  <div className='bottom'>
 
-                    </div>
                   </div>
                 </div>
               </div>
+            </div>
           </div>
         ) :
         (
